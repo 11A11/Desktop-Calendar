c7e312b6ae70ef409f3cacfe54d357a7cca1d7b2
Anadi Sharma <anadisharma11041997@gmail.com>
2017-06-23 17:46:12 +0530

    Delete resizer.py

--- resizer.py
+++
@@ -1,9 +0,0 @@
-import PIL
-from PIL import Image
-
-basewidth = 300
-img = Image.open('test.png')
-wpercent = (basewidth/float(img.size[0]))
-hsize = int((float(img.size[1])*float(wpercent)))
-img = img.resize((1366,hsize), PIL.Image.ANTIALIAS)
-img.save('test.png') 
